5d857ee8884dcae98a627f21eebcf0e92c5e92a5
Matthew Webber <matt@Matthews-iMac.local>
2020-04-03 16:56:26 -0400

    minor tweaks / reductions in main.py while rolling out an update for Hunter

--- main.py
+++ main.py
@@ -15,8 +15,15 @@ from direct_commands import echo_bomb, sound_bomb
 # this happens when calling "add contact" with 007 as the argument for "number"
 
 
-
 # todo ******make the below object methods******
+
+def provider_refresh(dx_, favorites):
+    print(f'Updating {dx_.name}...')
+    dx_.delete_all_contacts()
+    dx_.delete_callhistory()
+    dx_.add_all_favorites(favorites=favorites)
+    dx_.display_covid_alert(path=alert_path)
+
 
 class DummyDX:
 
@@ -31,8 +38,8 @@ def clear_all_alerts(DXs):
         DX_.display_alert('')
 
 
-def point_all_to_CUCM(DXs):
-    for DX_ in DXs:
+def point_all_to_cucm(dx_list):
+    for DX_ in dx_list:
         DX_.set_external_manager_address('128.23.1.36')
         DX_.set_provisioning_mode('CUCM')
         DX_.reboot()
@@ -49,20 +56,17 @@ def point_all_to_CUCM(DXs):
 
 # myDX = DX('10.27.200.140', password='')
 
-# # update group 1
-# DX_TELEPOD_01 = DX('10.33.114.35')
+print('\nLogging into telepod...\n')
+DX_TELEPOD_01 = DX('10.33.114.35')
 
 # # update group 2
-print('Logging into group 2...')
+print('\nLogging into nurse stations...\n')
 DX_NS_01 = DX('10.33.110.119')
-# DX_NS_02 = DX('10.33.112.74')
 DX_NS_02 = DummyDX('10.33.112.74', 'DX-NS-02', '40178@cucm.musc.edu')
 DX_NS_03 = DX('10.33.69.192')
-# DX_NS_03 = DummyDX('10.33.69.192', 'DX-NS-03', '40166@cucm.musc.edu')
 DX_NS_04 = DX('10.33.80.108')
 DX_NS_05 = DX('10.33.72.81')
 DX_NS_06 = DX('10.33.58.244')
-# DX_NS_06 = DummyDX('10.33.58.244', 'DX-NS-06', '40193@cucm.musc.edu')
 DX_NS_07 = DX('10.33.49.50')
 DX_NS_08 = DX('10.33.72.103')
 DX_NS_09 = DX('10.33.20.6')  # ex DX Patient 4
@@ -73,56 +77,83 @@ DX_NS_13 = DX('10.33.126.200')
 DX_NS_14 = DX('10.33.121.109')
 DX_NS_15 = DX('128.23.200.158')
 DX_NS_16 = DX('128.23.200.189')
+DX_5C_NS_01 = DX('10.33.46.85')
+DX_5C_NS_02 = DummyDX('10.33.121.109', 'DX-5C-NS-02', '40203@cucm.musc.edu')
 
-# print('Logging into ID station...')
-# DX_ID_NS_01 = DX('10.33.46.85')
+# DX_NS_02 = DX('10.33.112.74')
+# DX_NS_03 = DummyDX('10.33.69.192', 'DX-NS-03', '40166@cucm.musc.edu')
+# DX_NS_06 = DummyDX('10.33.58.244', 'DX-NS-06', '40193@cucm.musc.edu')
 
-NS_group = [DX_NS_01,
-            # DX_NS_02,
-            DX_NS_03,
-            DX_NS_04,
-            DX_NS_05,
-            DX_NS_06,
-            DX_NS_07,
-            DX_NS_08,
-            DX_NS_09,
-            DX_NS_10,
-            DX_NS_11,
-            DX_NS_12,
-            DX_NS_13,
-            DX_NS_14,
-            DX_NS_15,
-            DX_NS_16]
+NS_refresh_group = [DX_NS_01,
+                    # DX_NS_02,
+                    DX_NS_03,
+                    DX_NS_04,
+                    DX_NS_05,
+                    DX_NS_06,
+                    DX_NS_07,
+                    DX_NS_08,
+                    DX_NS_09,
+                    DX_NS_10,
+                    DX_NS_11,
+                    DX_NS_12,
+                    DX_NS_13,
+                    DX_NS_14,
+                    DX_NS_15,
+                    DX_NS_16,
+                    DX_5C_NS_01,
+                    # DX_5C_NS_02,
+                    ]
+
+NS_contact_group = [DX_NS_01,
+                    DX_NS_02,
+                    DX_NS_03,
+                    DX_NS_04,
+                    DX_NS_05,
+                    DX_NS_06,
+                    DX_NS_07,
+                    DX_NS_08,
+                    DX_NS_09,
+                    DX_NS_10,
+                    DX_NS_11,
+                    DX_NS_12,
+                    DX_NS_13,
+                    DX_NS_14,
+                    DX_NS_15,
+                    DX_NS_16,
+                    DX_5C_NS_01,
+                    DX_5C_NS_02,
+                    ]
 
 # # favorites 1
-print('Logging into carts...')
-# CART_01 = DX('10.33.26.21')
-CART_01 = DummyDX('10.33.26.21', 'MONA-VPO-CART-01', '11001@cucm.musc.edu')
+print('\nLogging into carts...\n')
+CART_01 = DX('10.33.26.21')
 CART_02 = DX('10.33.9.189')
-# CART_02 = DummyDX('10.33.9.189', 'MONA-VPO-CART-02', '11002@cucm.musc.edu')  # offline 12:34 3/26
 CART_03 = DX('10.33.10.144')
-# CART_03 = DummyDX('10.33.10.144', 'MONA-VPO-CART-03', '11003@cucm.musc.edu')
-# CART_04 = DummyDX('10.33.111.118', 'MONA-VPO-CART-04', '11004@cucm.musc.edu')
 CART_04 = DX('10.33.111.118')
 CART_05 = DX('10.33.58.239')
 CART_06 = DX('10.33.102.141')
 CART_07 = DX('10.33.107.22')
-# CART_07 = DummyDX('10.33.107.22', 'MONA-VPO-CART-07', '11007@cucm.musc.edu')
 CART_08 = DX('10.33.72.38')
 CART_09 = DX('10.33.84.116')
 CART_10 = DX('10.33.107.207')
 CART_11 = DX('10.33.102.191')
 CART_12 = DX('10.33.1.119')
 CART_13 = DX('10.33.126.42')
-# CART_14 = DX('10.33.45.45')
-CART_14 = DummyDX('10.33.45.45', 'MONA-VPO-CART-14', '11014@cucm.musc.edu')
+CART_14 = DX('10.33.45.45')
 CART_15 = DX('10.33.76.80')
 CART_16 = DX('10.33.2.188')
 # CART_17 = DX('10.33.62.71')
-CART_17 = DummyDX('10.33.62.71', 'MONA-VPO-CART-17', '11017@cucm.musc.edu')
 CART_18 = DX('10.33.31.218', password='')  # todo change pw to admin456
 CART_19 = DX('10.33.117.19')
 CART_20 = DX('10.33.87.102')
+
+# CART_01 = DummyDX('10.33.26.21', 'MONA-VPO-CART-01', '11001@cucm.musc.edu')
+# CART_02 = DummyDX('10.33.9.189', 'MONA-VPO-CART-02', '11002@cucm.musc.edu')  # offline 12:34 3/26
+# CART_03 = DummyDX('10.33.10.144', 'MONA-VPO-CART-03', '11003@cucm.musc.edu')
+# CART_04 = DummyDX('10.33.111.118', 'MONA-VPO-CART-04', '11004@cucm.musc.edu')
+# CART_07 = DummyDX('10.33.107.22', 'MONA-VPO-CART-07', '11007@cucm.musc.edu')
+# CART_14 = DummyDX('10.33.45.45', 'MONA-VPO-CART-14', '11014@cucm.musc.edu')
+CART_17 = DummyDX('10.33.62.71', 'MONA-VPO-CART-17', '11017@cucm.musc.edu')
 
 carts_only = [
     CART_01,
@@ -148,54 +179,38 @@ carts_only = [
 ]
 
 # favorites 2
-# print('Logging into patient DXs...')
-# patientDX1 = DX('10.33.59.0')
-# patientDX2 = DX('10.33.39.69')
-# patientDX3 = DX('10.33.18.168')
-# patientDX4 = DX('10.33.1.143')
-# patientDX5 = DX('10.33.81.103')
-# patientDX6 = DX('10.33.31.227')
-# patientDX7 = DX('10.33.120.174')
-# patientDX8 = DX('10.33.27.113')
-# patientDX10 = DummyDX('10.33.100.145', 'DX-PATIENT-10', '40162@cucm.musc.edu')  # registered but can't hit webUI
-# patientDX12 = DX('10.33.45.139')
+print('\nLogging into patient DXs...\n')
+patientDX1 = DX('10.33.59.0')
+patientDX2 = DX('10.33.39.69')
+patientDX3 = DX('10.33.18.168')
+patientDX4 = DX('10.33.1.143')
+patientDX5 = DX('10.33.81.103')
+patientDX6 = DX('10.33.31.227')
+patientDX7 = DX('10.33.120.174')
+patientDX8 = DX('10.33.27.113')
+patientDX10 = DummyDX('10.33.100.145', 'DX-PATIENT-10', '40162@cucm.musc.edu')  # registered but can't hit webUI
+patientDX12 = DX('10.33.45.139')
 
+patient_DXs = [
+    patientDX1,
+    patientDX2,
+    patientDX3,
+    patientDX4,
+    patientDX5,
+    patientDX6,
+    patientDX7,
+    patientDX8,
+    patientDX10,
+    patientDX12
+]
 
-# patient_DXs = [patientDX1, patientDX2, patientDX3, patientDX4, patientDX5, patientDX6, patientDX7, patientDX8, patientDX10, patientDX12]
-# all_pt_endpoints = carts_only + patient_DXs
-
-# DX_TRIAGE_01 = DX('10.33.81.103')
-# DX_TRIAGE_02 = DX('128.23.2.27')
-# DX_TRIAGE_03 = DX('128.23.200.229')
-# DX_TRIAGE_04 = DX('128.23.200.195')
-
-musc_vidyo = '112453@vidyo.musc.edu'
-pspn_vidyo = '102007@vidyo.pspnsc.org'
-
-# mylist = myDX.phonebook_search('PATIENT')
-# when the list of contacts get's passed back....
-    # for elem in mylist:
-    #     myDX.delete_contact(elem.find('ContactId').text)
+all_pt_endpoints = carts_only + patient_DXs
 
 alert_path = '/Users/matt/PycharmProjects/dx_sx_api/DX_alert_msg.txt'  # home iMac
-#
-# print(f'Updating {DX_TELEPOD_01.name}...')
-# DX_TELEPOD_01.delete_all_contacts()
-# DX_TELEPOD_01.add_all_favorites(favorites=all_pt_endpoints)
-# DX_TELEPOD_01.display_covid_alert(path=alert_path)
-#
-# print(f'Updating {DX_ID_NS_01.name}...')
-# DX_ID_NS_01.delete_all_contacts()
-# DX_ID_NS_01.add_all_favorites(favorites=patient_DXs)
-# DX_ID_NS_01.display_covid_alert(path=alert_path)
 
-for dx_ in NS_group:
-    print(f'Updating {dx_.name}...')
-    dx_.delete_all_contacts()
-    dx_.delete_callhistory()
-    dx_.add_all_favorites(favorites=carts_only)
-    dx_.display_covid_alert(path=alert_path)
+print('\nUpdating telepod...\n')
+provider_refresh(DX_TELEPOD_01, favorites=all_pt_endpoints + NS_contact_group)
 
-
-# todo - add favorites to NS03 / 06 when online
-
+print('\nUpdating nurse stations...\n')
+for dx_ in NS_refresh_group:
+    provider_refresh(dx_, favorites=all_pt_endpoints + NS_contact_group)
